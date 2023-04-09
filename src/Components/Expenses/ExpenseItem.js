6d496311eda19f1ba4b8c0f60cef10544c519dab
Tushar <tush.sharma2094@gmail.com>
2023-04-09 14:05:04 +0530

    Modified styling

---
+++ src/Components/Expenses/ExpenseItem.js
@@ -0,0 +1,21 @@
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+import { useState } from 'react';
+
+export default function ExpenseItem(props) {
+  const [amount, Updater] = useState(props.amount);
+  const clickHandler = () => {
+    Updater('∞');
+  }
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>₹{amount}
+        <button onClick={clickHandler}>Change Price</button></div>
+      </div>
+    </Card>
+  );
+}
---
+++ src/Components/NewExpenses/NewExpense.js
@@ -0,0 +1,11 @@
+import "./NewExpense.css";
+import FORMMMM from "./ExpenseForm";
+const NewExpense = () => {
+  return (
+    <div className="new-expense">
+        <FORMMMM />
+    </div>
+  );
+};
+
+export default NewExpense;
