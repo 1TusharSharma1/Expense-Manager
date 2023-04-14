5dc341eb83b535d1f4902be0a00643408c5f1c83
Tushar <tush.sharma2094@gmail.com>
2023-04-14 23:40:08 +0530

    Completed my project

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,21 +1,19 @@
+import React from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
-import { useState } from 'react';
 
-export default function ExpenseItem(props) {
-  const [amount, Updater] = useState(props.amount);
-  const clickHandler = () => {
-    Updater('∞');
-  }
+const ExpenseItem = (props) => {
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>₹{amount}
-        <button onClick={clickHandler}>Change Price</button></div>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </Card>
   );
-} 
+}
+
+export default ExpenseItem;
